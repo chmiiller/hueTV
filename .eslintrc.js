2a26df11c283f5664687003bc5195019586336de
Carlos Zinato <carlosh.lz@hotmail.com>
2023-05-28 18:01:27 +0200

    fix indent on .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,36 @@
-  module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-indent": ["warn", 2],
+    "react/jsx-indent-props": ["warn", 2],
+    "react/jsx-props-no-spreading": [0],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    indent: ["warn", 2],
+    "linebreak-style": ["warn", "unix"],
+    semi: ["warn", "always"],
+    "no-unused-vars": [
+      "warn",
+      { vars: "all", args: "none", ignoreRestSiblings: false },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'react/jsx-indent': ['warn', 4],
-        'react/jsx-indent-props': ['warn', 4],
-        'react/jsx-props-no-spreading': [0],
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        indent: ['warn', 2],
-        'linebreak-style': [
-            'warn',
-            'unix',
-        ],
-        semi: [
-            'warn',
-            'always',
-        ],
-        'no-unused-vars': [
-            'warn', { vars: 'all', args: 'none', ignoreRestSiblings: false },
-        ],
-    }
-  };
+  },
+};
