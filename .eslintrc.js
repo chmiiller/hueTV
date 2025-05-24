014373b2df807c0b012eabc6d1783ef8cbb1e970
Carlos Zinato <carlosh.lz@hotmail.com>
2025-05-24 12:40:58 +0200

    removed indent lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,6 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "warn",
-    indent: ["warn", 2],
     "linebreak-style": ["warn", "unix"],
     semi: ["warn", "always"],
     "no-unused-vars": [
